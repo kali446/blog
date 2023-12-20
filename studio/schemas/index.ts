b2f98c82c668bc472fec52082ffb9cb704b34425
Manzil Junior <clonedbeast@Manzils-MacBook-Air.local>
2023-12-20 19:22:18 +0545

    studio removed

--- studio/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import category from './category'
-import article from './article'
-import tag from './tag'
-import author from './author'
-import homeSectionArticle from './homeSectionArticle'
-import PickedArticles from './sidebarSectionArticle'
-
-export const schemaTypes = [category, article, tag, author, homeSectionArticle, PickedArticles]
